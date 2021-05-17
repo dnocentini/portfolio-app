d5abb43ef4f5c74e135a751d3d47eb0a45273503
Dinny Nocentini <dinny.nocentini@gmail.com>
2021-05-17 15:24:27 -0500

    add onClick toggle on SidebarLinks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -18,10 +18,10 @@ const Sidebar = ({isOpen, toggle}) => {
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to="home">Home</SidebarLink>
-                    <SidebarLink to="about">About</SidebarLink>
-                    <SidebarLink to="project">Project</SidebarLink>
-                    <SidebarLink to="contact">Contact</SidebarLink>
+                    <SidebarLink to="home" onClick={toggle}>Home</SidebarLink>
+                    <SidebarLink to="about" onClick={toggle}>About</SidebarLink>
+                    <SidebarLink to="project" onClick={toggle}>Project</SidebarLink>
+                    <SidebarLink to="contact" onClick={toggle}>Contact</SidebarLink>
                 </SidebarMenu>
                 <SideBtnWrap>
                     <SidebarRoute to='/resume'>Resume</SidebarRoute>
